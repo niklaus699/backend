80c28825139d958b81022d496ab3362539c94051
niklaus699 <nicholasbasseyy@gmail.com>
2026-04-20 21:35:20 +0100

    fix ssl issues with redis

--- config/settings/production.py
+++ config/settings/production.py
@@ -53,6 +53,9 @@ CACHES = {
         'LOCATION': os.getenv('CACHE_URL', os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1')),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'CONNECTION_POOL_KWARGS': {
+                'ssl_cert_reqs': None
+            }
         },
     }
 }
